dbfa551b6f1d71c1737d7a4d53e515d5a70b8650
StreamsYT <37671311+StreamsYT@users.noreply.github.com>
2022-11-13 09:19:01 +0100

    Init

---
+++ WorkshopTestIntApp/WorkshopTestIntApp_IntTest/IntTest.cs
@@ -0,0 +1,13 @@
+namespace WorkshopTestIntApp_IntTest
+{
+    [TestClass]
+    public class IntTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Thread.Sleep(10000);
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ WorkshopTestIntApp/WorkshopTestIntApp_UnitTest/StandardUnitTests.cs
@@ -0,0 +1,19 @@
+namespace WorkshopTestIntApp_UnitTest
+{
+    [TestClass]
+    public class StandardUnitTests
+    {
+
+        [TestMethod]
+        public void WorkingTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void FailingTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
